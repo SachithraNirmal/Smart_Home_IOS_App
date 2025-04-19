ac9fd05639b862aef96840ce0902afb50ed54c91
Sachithra Nirmal <sachi.nirmal97@gmail.com>
2025-04-19 13:35:00 +0530

    Initial Commit

---
+++ New IOS App_1/New_IOS_App_1App.swift
@@ -0,0 +1,17 @@
+//
+//  New_IOS_App_1App.swift
+//  New IOS App_1
+//
+//  Created by Sachithra on 2025-03-22.
+//
+
+import SwiftUI
+
+@main
+struct New_IOS_App_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New IOS App_1Tests/New_IOS_App_1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  New_IOS_App_1Tests.swift
+//  New IOS App_1Tests
+//
+//  Created by Sachithra on 2025-03-22.
+//
+
+import Testing
+
+struct New_IOS_App_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
